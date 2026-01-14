3db8724d3ef44a3d9a4416716ca2b8c3ca6d9a06
Hari Saurabh Yadav <63455651+HARISAURABHYADAV@users.noreply.github.com>
2026-01-14 15:42:51 +0000

    arithmatic operation

---
+++ 01_basics/05_operations.js
@@ -0,0 +1,23 @@
+let value = 5 
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2 + 2);
+// console.log(5 - 2);
+// console.log(5 * 2);
+// console.log(5 **2 ); // ** means power
+// console.log(5 / 2); // devide => 2.5
+// console.log(5 % 2); // remender => 1
+
+let str1 = "hello ";
+let str2 = "Hari"
+let str3 = str1 + str2;
+// console.log(str3)
+
+// preferedType take as first parameter
+console.log("1" + 2 + 3); // 122 =>  "1" string 
+console.log(1 + 2 + "3"); // 33 => 1 as number 
+
+
+
+
