7518e7813a22b30bfd6bd53e8d174731edd5f7ed
Hari Saurabh Yadav <63455651+HARISAURABHYADAV@users.noreply.github.com>
2026-01-14 15:16:37 +0000

    dataTypes

---
+++ 01_basics/03_datTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // use newer version of the JS
+
+console.log(3 
+    + 
+    3
+)  // code readability should be high 
+
+// premitive data types
+let name = "Hari" // string 
+let age = 20 //number
+let isLoggedIn = true // boolean => true or false
+let accountCity //undefined => value is not assigned 
+let accountId = null; // null => standalone value // it is also a data type
+
+//symbol ==> for use for uniqueness of the variable
+
+//objects
+console.log(typeof "hari")
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+//dataType conversion 
